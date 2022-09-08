3131af6883bdcbf96022a921a1f1da14ff4a5fcc
Howard-12 <s0989175085@gmail.com>
2022-09-08 11:08:30 +1000

    update

--- p5_projects/1.colour_with_difference_alpha/sketch.js
+++ p5_projects/1.colour_with_difference_alpha/sketch.js
@@ -10,8 +10,22 @@ function setup() {
 function draw() {
   background(100, 104, 251);
 
-  for (let i = 0; i < 6; ++i) {
-    fill(104, 203, 152, 255/5*(i+1));
-    rect(width/5*i + 15, height/2 - rectSize/2 , rectSize, rectSize, 20);
-  }
+  // for (let i = 0; i < 6; ++i) {
+  //   fill(104, 203, 152, 255/5*(i+1));
+  //   rect(width/5*i + 15, height/2 - rectSize/2 , rectSize, rectSize, 20);
+  // }
+  fill(104, 203, 152, 255/5*1);
+  rect(width/5*0 + 15, height/2 - rectSize/2 , rectSize, rectSize, 20);
+
+  fill(104, 203, 152, 255/5*2);
+  rect(width/5*1 + 15, height/2 - rectSize/2 , rectSize, rectSize, 20);
+
+  fill(104, 203, 152, 255/5*3);
+  rect(width/5*2 + 15, height/2 - rectSize/2 , rectSize, rectSize, 20);
+
+  fill(104, 203, 152, 255/5*4);
+  rect(width/5*3 + 15, height/2 - rectSize/2 , rectSize, rectSize, 20);
+
+  fill(104, 203, 152, 255/5*5);
+  rect(width/5*4 + 15, height/2 - rectSize/2 , rectSize, rectSize, 20);
 }
