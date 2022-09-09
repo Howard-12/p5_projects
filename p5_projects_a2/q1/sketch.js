5060d6d321aa0d140fa5435ca518acb26f065ddb
Howard-12 <s0989175085@gmail.com>
2022-09-09 16:35:40 +1000

    update

--- p5_projects_a2/q1/sketch.js
+++ p5_projects_a2/q1/sketch.js
@@ -1,31 +1,67 @@
-let s;
+let sT;
 let track;
-let p=[];
-let texture;
+let grass;
+let grassTile;
+
 
 function preload()
 {
-    texture = loadImage("assets/ground_mud_d.jpg", img => { img.width = 50; img.height = 50; });
+    grassTile = loadImage("assets/ground_mud_d.jpg", img => { img.width = 40; img.height = 40; });
 }
 
 function setup()
 {
-  createCanvas(750, 750);
-  fill(0);
-  s = createSprite(100,100,10,10);
-  s.setVelocity(1, 1);
-  s.addImage(texture);
+  createCanvas(600, 600);
+
+  grass = new Group();
+
+  // for (let i = 0; i < 15; i++)
+  // {
+  //   s = createSprite(i*40+20,100,10,10);
+  //   s.setVelocity(1, 1);
+  //   s.addImage(grassTile);
+  //   grass.add(s);
+  // }
   loadStrings("track.txt", (t) => {
-    for (i of t)
+    for (let i of t)
     {
-      for (j of i)
+      let tileC = 0;
+      for (let j of i)
       {
-        console.log(
-          j == "1" ? j : j == "0" ? j : j == "2" ? j : null
-        );
+        switch (j)
+        {
+          case "0":
+            console.log("0");
+          case "1":
+            console.log("1");
+            // let s = new Sprite(grassT*40+20,100,10,10);
+            // s.setVelocity(1, 1);
+            // s.addImage(grassTile);
+            // grass.add(s);
+            ++tileC;
+          case "2":
+            console.log("2");
+        }
+          // j == "1"
+          //   ?
+          //   console.log("adfsg")
+          //   : j == "0"
+          //     ? j
+          //     : j == "2"
+          //       ? j
+          //       : null
+
+        // console.log(t[i].length);
+        // console.log(
+        //   j == "1" ? j : j == "0" ? j : j == "2" ? j : null
+        // );
       }
     }
-    p = t;
+    let s = createSprite(1*40+20,1*40+20,10,10);
+    s.setVelocity(1, 1);
+    s.addImage(grassTile);
+    grass.add(s);
+
   });
   noLoop();
 }
@@ -35,11 +71,8 @@ function draw()
   background(250);
 
 
-
-  drawSprites();
-
-
-
+  grass.draw();
+  // drawSprites();
 }
 
 // function mousePressed() {
